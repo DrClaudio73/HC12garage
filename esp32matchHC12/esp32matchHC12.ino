73e5bfb7baa12e3682ab6f1778ff9b833652264f
Claudio Cattaneo <ccattaneo73@gmail.com>
2019-12-31 06:47:21 +0100

    renamed:    esp32matchHC12.ino -> esp32matchHC12/esp32matchHC12.ino

--- esp32matchHC12/esp32matchHC12.ino
+++ esp32matchHC12/esp32matchHC12.ino
@@ -10,9 +10,9 @@ const byte bigGateControlPin = 5;
 const byte pressDuration = 1500;
 //================= PART FOR CONTOLLING GARAGE==========================
 //================= PART FOR HC12==========================
-const byte HC12RxdPin = 9; // Recieve Pin on HC12
+const byte HC12RxdPin = 9; // Receive Pin on HC12
 const byte HC12TxdPin = 8; // Transmit Pin on HC12
-const byte HC12SetPin = 7; // Transmit Pin on HC12
+const byte HC12SetPin = 7; // Set Pin on HC12
 
 //unsigned long timer = millis(); // Delay Timer
 char HC12ByteIn;    // Temporary variable
@@ -23,7 +23,7 @@ boolean SerialEnd = false;    // Flag to indicate End of Serial String
 boolean HC12End = false;      // Flag to indiacte End of HC12 String
 boolean commandMode = false;  // Send AT commands
 
-// Software Serial ports Rx and Tx are opposite the HC12 Rx and Tx
+// Software Serial ports Rx and Tx are opposite to the HC12 Rx and Tx pins
 // Create Software Serial Port for HC12
 SoftwareSerial HC12(HC12TxdPin, HC12RxdPin);
 //================= PART FOR HC12==========================
@@ -44,11 +44,11 @@ void setup() {
   digitalWrite(garageControlPin, RelayOff);
   digitalWrite(bigGateControlPin, RelayOff);
 
-  HC12ReadBuffer.reserve(128);     // Reserve 64 bytes for Serial message input
+  HC12ReadBuffer.reserve(128);    // Reserve 64 bytes for Serial message input
   pinMode(HC12SetPin, OUTPUT);    // Output High for Transparent mode / Low for Command mode
   digitalWrite(HC12SetPin, HIGH); // Enter Transparent mode
   delay(80);                      // 80 ms delay before operation per datasheet
-  Serial.begin(57600);             // Open serial port to computer
+  Serial.begin(57600);            // Open serial port to computer
   HC12.begin(2400);               // Open software serial port to HC12
 
   Serial.println("Hello from Garage Pro Mini!!!!!!");             // Hello message!
@@ -59,10 +59,10 @@ void setup() {
 
 void loop()
 {
-  while (HC12.available()) {  // While Arduino's HC12 soft serial rx buffer has data
-    HC12ByteIn = HC12.read(); // Store each character from rx buffer in byteIn
-    if (HC12ByteIn == '\n') { // At the end of the line
-      HC12End = true; //Set HC12End flag to true
+  while (HC12.available()) {              // While Arduino's HC12 soft serial rx buffer has data
+    HC12ByteIn = HC12.read();             // Store each character from rx buffer in byteIn
+    if (HC12ByteIn == '\n') {             // At the end of the line
+      HC12End = true;                     //Set HC12End flag to true
     } else {
       HC12ReadBuffer += char(HC12ByteIn); // Write each character of byteIn to HC12ReadBuffer
     }
@@ -72,27 +72,28 @@ void loop()
     Serial.print("Received: ");
     Serial.print(HC12ReadBuffer.length());
     Serial.print(" bytes. Content: ");
-    Serial.println(HC12ReadBuffer);     // Send received message to screen
+    Serial.println(HC12ReadBuffer);       // Send received message to screen
     Serial.print("Reply with ACK+");
     Serial.print(HC12ReadBuffer);
     Serial.println("+!");
-    for (int k=4;k>=0;k--){
-      HC12.print("ACK");                  // Reply with ACK+same message +"!"
-      HC12.print(HC12ReadBuffer);         // Reply with ACK+same message +"!"
-      HC12.println("!");                  // Reply with ACK+same message +"!"      
+    for (int k=4;k>=0;k--){ //Sends 5 ACK messages to sender station
+      HC12.print("ACK");                  // Reply with "ACK+same message as received+!"
+      HC12.print(HC12ReadBuffer);         // Reply with "ACK+same message as received+!"
+      HC12.println("!");                  // Reply with "ACK+same message as received+!"
     }
     
     String passaON("Ciao: 4!");
     String passaOFF("Ciao: 6!");
     String passaApriSoloGrande("Ciao: 7!");
     String passaApriTutto("Ciao: 9!");
+    
     if (!HC12ReadBuffer.compareTo(passaON)){
       ledStatus=1;
       digitalWrite(garageControlPin,RelayOn);
       delay(pressDuration);
       digitalWrite(garageControlPin,RelayOff);
       presentationBlink();
-      Serial.println("Match ok! Opening Only Garage and Switching ON!!!!");
+      Serial.println("Match ok! Opening Only Garage and Switching ON ledPin!!!!");
       digitalWrite(ledPin,ledStatus);
     } else if (!HC12ReadBuffer.compareTo(passaOFF)) {
       ledStatus=0;
@@ -100,7 +101,7 @@ void loop()
       delay(pressDuration);
       digitalWrite(garageControlPin,RelayOff);
       presentationBlink();
-      Serial.println("Match ok! Opening Only Garage and Switching OFF!!!!");
+      Serial.println("Match ok! Opening Only Garage and Switching OFF ledPin!!!!");
       digitalWrite(ledPin,ledStatus);
     } else if (!HC12ReadBuffer.compareTo(passaApriSoloGrande)) {
       ledStatus=0;
@@ -132,6 +133,5 @@ void loop()
     
     HC12ReadBuffer = ""; // Empty buffer
     HC12End = false; // Reset flag
-    
   }
 }
